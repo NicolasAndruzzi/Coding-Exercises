c4d6458a4dd8184ab29673274d69b6906e4d9c5f
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-05-28 18:49:07 -0600

    inital commit, starting challenge 1

---
+++ Challenge-1/converter.js
@@ -0,0 +1,12 @@
+// Write some code that will accept an amount and convert it to the
+// appropriate string representation.
+//
+// Example:
+// Convert 2523.04
+// to "Two thousand five hundred twenty-three and 04/100dollars"
+
+var input = "2523.04"
+
+function convert(value) {
+  
+}
