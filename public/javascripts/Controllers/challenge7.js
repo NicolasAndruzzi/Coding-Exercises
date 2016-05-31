efe11382c2cda13961dd95444cf73eb5fa0cb5a3
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-05-31 05:46:11 -0600

    all 5 challenges added and  re-styled

--- public/javascripts/Controllers/challenge7.js
+++ public/javascripts/Controllers/challenge7.js
@@ -1,3 +1,27 @@
 app.controller("challenge7Controller", function($scope){
   console.log("challenge7Controller");
+
+  // Will calculate any number in the fibonacci sequence that is sooner than the 1477th position
+
+  // var inputIndex = 2;
+
+  $scope.inputValueFibonacci = 0;
+  $scope.change = function (desiredIndex) {
+    if ($scope.inputValueFibonacci > 1476) {
+      $scope.inputValueFibonacci = 0;
+    }
+
+    fibonacciSequnce = [0,1];
+
+    for (var i = 0; i < desiredIndex; i++) {
+      fibonacciSequnce[i+2] = fibonacciSequnce[i]+fibonacciSequnce[i+1]
+    }
+    return fibonacciSequnce[desiredIndex]
+  };
+
+  // console.time('time');
+  // console.log(fibonacci(inputIndex));
+  // console.timeEnd('time');
+
+
 });
